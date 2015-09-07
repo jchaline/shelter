65dff222ef260e0918725312e968b6a122faf6c1
Jeremy Chaline <jchaline@users.noreply.github.com>
2015-09-07 16:36:05 +0200

    Correct special init

--- src/main/java/fr/jchaline/shelter/config/WebConfig.java
+++ src/main/java/fr/jchaline/shelter/config/WebConfig.java
@@ -1,7 +1,6 @@
 package fr.jchaline.shelter.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -9,12 +8,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 	
-	/**
-	 * Access allowed for territory controller list method
-	 */
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/territory/list");
-		registry.addMapping("/road/list");
-	}
 }
--- src/main/java/fr/jchaline/shelter/domain/Special.java
+++ src/main/java/fr/jchaline/shelter/domain/Special.java
@@ -1,13 +1,17 @@
 package fr.jchaline.shelter.domain;
 
+import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.MapKey;
-import javax.persistence.OneToMany;
+import javax.persistence.EnumType;
+import javax.persistence.FetchType;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.MapKeyEnumerated;
 import javax.persistence.Table;
 
 import fr.jchaline.shelter.utils.SpecialEnum;
@@ -15,31 +19,38 @@ import fr.jchaline.shelter.utils.SpecialEnum;
 @Entity
 @Table
 public class Special extends AbstractEntity {
-	
-	@OneToMany(cascade = CascadeType.ALL)
-	@MapKey
+
+	@ElementCollection(fetch = FetchType.EAGER)
+	@MapKeyColumn(name = "special")
+	@MapKeyEnumerated(EnumType.STRING)
+	@Column(name = "value")
 	private Map<SpecialEnum, Integer> values;
 
 	public Special(List<Integer> special) {
 		int i = 0;
 		values = new HashMap<SpecialEnum, Integer>();
-		values.put(SpecialEnum.S, i++);
-		values.put(SpecialEnum.P, i++);
-		values.put(SpecialEnum.E, i++);
-		values.put(SpecialEnum.C, i++);
-		values.put(SpecialEnum.I, i++);
-		values.put(SpecialEnum.A, i++);
-		values.put(SpecialEnum.L, i++);
+		values.put(SpecialEnum.S, special.get(i++));
+		values.put(SpecialEnum.P, special.get(i++));
+		values.put(SpecialEnum.E, special.get(i++));
+		values.put(SpecialEnum.C, special.get(i++));
+		values.put(SpecialEnum.I, special.get(i++));
+		values.put(SpecialEnum.A, special.get(i++));
+		values.put(SpecialEnum.L, special.get(i++));
 	}
 
 	public Special() {
-		
+
 	}
 	
+	public String toString() {
+		return MessageFormat.format("S({0})P({1})E({2})C({3})I({4})A({5})L({6})", values.get(SpecialEnum.S),values.get(SpecialEnum.P),values.get(SpecialEnum.E),
+				values.get(SpecialEnum.C),values.get(SpecialEnum.I),values.get(SpecialEnum.A), values.get(SpecialEnum.L));
+	}
+
 	public int getValue(SpecialEnum special) {
 		return values.get(special);
 	}
-	
+
 	public void setValue(SpecialEnum special, int value) {
 		this.getValues().put(special, value);
 	}
@@ -51,5 +62,5 @@ public class Special extends AbstractEntity {
 	public void setValues(Map<SpecialEnum, Integer> values) {
 		this.values = values;
 	}
-	
+
 }
